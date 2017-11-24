4c9dfc8dd0bfbfc77bee762eca1359cfd7e8cfbc
barbarafv <32398342+barbarafv@users.noreply.github.com>
2017-11-24 21:45:49 -0200

    Update Parte1.s

--- Parte1.s
+++ Parte1.s
@@ -1,14 +1,14 @@
 # Trabalho-ARM
 	
-		MOV		R0, #1 ; a1
-		MOV		R1, #0 ; n
+		MOV	  R0, #1 ; a1
+		MOV	  R1, #1 ; n
 		MOV       R2, #0 ; an
 		MOV       R3, #2 ; q
 		
 FOR
 		CMP		R1, #10
 		BEQ		DONE
-		 		R1, R1, R0
-		ADD		R0, R0, #1
+		LSL 		R3, R3, #1
+		ADD		R1, R1, #1
 		B		FOR
 DONE
