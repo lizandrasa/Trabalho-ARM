a84a7656e75f2a4ad0ac774d98b1012359084a94
lizandrasa <32456413+lizandrasa@users.noreply.github.com>
2017-11-29 00:50:50 -0200

    Update Parte1.s

--- Parte1.s
+++ Parte1.s
@@ -1,8 +1,7 @@
-# Trabalho-ARM
-		MOV		R1, #1 ; n
-		MOV		R4, #2147483648	;coloca em R6 o valor para TST
-		MOV		R6, #5 ;numero base (a1)
-		MOV		R7, #500 ; RX*10
+		ADD		R1, #1 ; n
+		ADD		R4, #2147483648	;coloca em R6 o valor para TST
+		ADD		R6, #5 ;numero base (a1)
+		ADD		R7, #500 ; RX*10
 		STR		R6,[R7,#0] ; armazena o numero base no endereço #500
 		
 		
@@ -17,4 +16,4 @@ FOR
 		TST		R4, R6 ;testa se o valor de R6 é inferior a 2147483648
 		B		FOR
 DONE
-                LDR          R5,[R7,#0] ; armazena o ultimo valor da PG em R5
+		LDR		R5,[R7,#0] ; armazena o ultimo valor da PG em R5
